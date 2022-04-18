35666d35b9496dc3a9a1b2e658aa87ddb26a2b8f
Vog2k <99256921+Vog2k@users.noreply.github.com>
2022-04-18 22:10:00 +1200

    14/04/2022

---
+++ to do list.bat
@@ -0,0 +1,22 @@
+to do list
+
+try get the Services All the same size
+link the tow buttons on the main screen
+add click to logo/home
+replace main photo for better 2 photos 
+set up links for logos at the bottem
+set up links for locations
+still not sure 
+delete enroll section
+homepage = need to replace about us rubbish with writting
+change names for reviews
+add photos for reviews
+add somthing the top right of the banner 
+
+
+set up calender page
+
+"contact us"
+Add phone number/name/email. with two banners head and footer then done.
+
+"About us"
