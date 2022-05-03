2d331a82b69c439c102408524025c9f745bdc36b
Tim <99256921+Vog2k@users.noreply.github.com>
2022-05-03 21:21:08 +1200

    finished home page

--- TESTING.js
+++ TESTING.js
@@ -1,11 +1,69 @@
-var body=document.getElementsByTagName("body")[0];
-var parent=document.getElementById("parent-div");
-var child=document.getElementById("child-div");
-body.onscroll = function(){
-//console.log(documenhttps://fiddle.jshell.net/3urv0tp0/#tidyt.getElementById("child-div").style.top)
-if(document.documentElement.scrollTop>=child.offsetTop)//Adjust Tolerance as you want
-{
-   child.style.display="block"
-}
-
-};
+$(document).ready(function() {
+   // Check if element is scrolled into view
+   function isScrolledIntoView(elem) {
+     var docViewTop = $(window).scrollTop();
+     var docViewBottom = docViewTop + $(window).height();
+ 
+     var elemTop = $(elem).offset().top;
+     var elemBottom = elemTop + $(elem).height();
+ 
+     return elemBottom <= docViewBottom && elemTop >= docViewTop;
+   }
+   // If element is scrolled into view, fade it in
+   $(window).scroll(function() {
+     $(".scroll-animations .animated").each(function() {
+       if (isScrolledIntoView(this) === true) {
+         $(this).addClass("fadeInLeft");
+       }
+     });
+   });
+ 
+   // Click Animations
+   $("button").on("click", function() {
+     /*
+     If any input is empty make it's border red and shake it. After the animation is complete, remove the shake and animated classes so that the animation can repeat.
+     */
+ 
+     // Check name input
+     if ($("#name").val() === "") {
+       $("#name")
+         .addClass("form-error animated shake")
+         .one(
+           "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+           function() {
+             $(this).removeClass("animated shake");
+           }
+         );
+     } else {
+       $("#name").removeClass("form-error");
+     }
+ 
+     // Check email input
+     if ($("#email").val() === "") {
+       $("#email")
+         .addClass("form-error animated shake")
+         .one(
+           "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+           function() {
+             $(this).removeClass("animated shake");
+           }
+         );
+     } else {
+       $("#email").removeClass("form-error");
+     }
+ 
+     // Check message textarea
+     if ($("#message").val() === "") {
+       $("#message")
+         .addClass("form-error animated shake")
+         .one(
+           "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+           function() {
+             $(this).removeClass("animated shake");
+           }
+         );
+     } else {
+       $("#message").removeClass("form-error");
+     }
+   });
+ });
